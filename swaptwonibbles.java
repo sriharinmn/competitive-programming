ec32b1ff00e967665f6cfce758c762ad42c131fb
NightsBright <sriharik2019@gmail.com>
2025-11-17 09:49:21 +0530

    bit operation on swapping two nibbles

--- swaptwonibbles.java
+++ swaptwonibbles.java
@@ -1,22 +1,10 @@
-import java.util.*;
+public class SwapNibbles {
+    public static int swapNibbles(int x) {
+        return ((x & 0x0F) << 4) | ((x & 0xF0) >> 4);
+    }
 
-public class Main{
-    public static void main(String[] args){
-
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-
-        int binary = String.format("%8s", Integer.toBinaryString(n)).replace(' ', '0');
-
-        String leftnibble = binary(0,4);
-        String rightnibble = binary(4);
-
-        String swapper = rightnibble + leftnibble;
-
-        System.out.printf("original number : %d\n", n);
-        System.out.printf("after swapping : %d\n", Integer.parseInt(swapper,2));
-        System.out.printf("binary representation : %s\n", binary);
-        System.out.printf("after swapping binary representation : %s\n", swapper);
+    public static void main(String[] args) {
+        int x = 100; // example
+        System.out.println(swapNibbles(x));
     }
 }
